964f696abe903caf03f440e17b30a5ef70d1cf8e
domasgru <dominykas.grubys@hostinger.com>
2021-01-23 11:22:43 +0200

    Load font  async

--- index.vue
+++ index.vue
@@ -4,10 +4,21 @@
       <meta charset="utf-8">
       <meta name="description" content="Personal feedback sharing platform">
       <title>Kuri - personal feedback sharing platform</title>
-      <link rel="preconnect" href="https://fonts.gstatic.com">
-      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <style v-html="css"/>
+      <script type="text/javascript">
+        WebFontConfig = {
+            google: { families: [ 'Inter:400,600,700' ] }
+        };
+        (function() {
+            var wf = document.createElement('script');
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+            wf.type = 'text/javascript';
+            wf.async = 'true';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(wf, s);
+        })(); 
+    </script>
     </head>
     <body>
       <landing />
